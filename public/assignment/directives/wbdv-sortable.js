b9c125a82d98c3fa153fba8556dca2d4384c4c53
chenxianp <chen.xianp@husky.neu.edu>
2017-06-05 18:42:38 -0400

    commit of 6/5, assignment 4 basically done

--- public/assignment/directives/wbdv-sortable.js
+++ public/assignment/directives/wbdv-sortable.js
@@ -1,11 +1,43 @@
 (function () {
     angular
-        .module('wbdvDirectives',[])
-        .directive('wdSortable', wdSortable);
+        .module('WAM')
+        .directive('wdDraggable', wdDraggable);
     
-    function wdSortable() {
+    function wdDraggable($http) {
+
         function linkFunction(scope, element) {
-            $(element).sortable();
+            var initialArray = [];
+            var afterArray = [];
+
+            $(element).sortable({start: function(event, item) {
+
+                initialArray = $(element).sortable( "toArray" );
+            }
+                });
+            $(element).sortable({stop: function(event, ui) {
+                afterArray = $(element).sortable("toArray");
+                console.log(afterArray);
+
+                for(i = 0; i < initialArray.length; i++) {
+                    if (initialArray[i] !== afterArray[i]) {
+                        var initial = i;
+                        var final = afterArray.indexOf(initialArray[i]);
+                        var url = '/api/page/'+321+'/widget?initial='+initial+'&final='+final;
+                        return $http.put(url)
+                            .then(function (response) {
+                                return response.data;
+                            });
+
+                    }
+                }
+
+            }
+            });
+
+
+
+
+
 
         }
         return {
